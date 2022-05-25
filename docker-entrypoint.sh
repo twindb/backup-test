dc38662339600da1fa3f80845283a6a5a1d90d01
Aleksandr Kuzminsky <aleksandr.kuzminsky@gmail.com>
2022-05-25 05:55:38 -0700

    Add a master image for Ubuntu/focal

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,5 +7,6 @@ mkdir -p ${DATADIR}
 mysqld --initialize-insecure --user=root
 /bin/chown -R mysql:mysql "${DATADIR}" /var/run/mysqld
 /bin/chmod 777 /var/run/mysqld
+/usr/sbin/sshd
 
 exec mysqld --user=root
